1065e47430865f177dfed9e73e5629b945431469
kendirliurunver <emrekendirli@urunver.com.tr>
2023-12-31 15:33:46 +0300

    works

--- api/routes/appointmentRoute.js
+++ api/routes/appointmentRoute.js
@@ -6,7 +6,7 @@ const router =express.Router()
 
 router.route("/").post(Auth.authenticatePatientAPIToken,FormData.uploadSettingImages,Appointment.create)
 router.route("/doctor-list").get(Auth.authenticateUserDoctorAPIToken,Appointment.bringDoctorActiveAppointments)
-router.route("/patient-list").get(Auth.authenticatePatientAPIToken,Appointment.bringDoctorActiveAppointments)
+router.route("/patient-list").get(Auth.authenticatePatientAPIToken,Appointment.bringPatientActiveAppointments)
 router.route("/:id").delete(Auth.authenticateUserDoctorAPIToken,Appointment.deleteAppointment)
 
 router.route("/doctor-hours-list/:id").get(Appointment.doctorAppointmentHours)
